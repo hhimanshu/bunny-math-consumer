f00f989c35343330645bf43149412fba39c27ef9
hhimanshu <dev.atwork@yahoo.com>
2024-04-11 10:59:50 +1200

    Add @hhimanshu/bunny-math-producer package as a dependency

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { add } from "@hhimanshu/bunny-math-producer";
 
 type Data = {
   name: string;
+  age: number;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const total = add(12, 23);
+  res.status(200).json({ name: "John Doe", age: total });
 }
